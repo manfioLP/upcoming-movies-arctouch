095fc50cb6554103f4aa09ae2ae3ca763a92d34f
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2019-11-03 21:15:59 -0200

    create base function to rest connection call

---
+++ api/handlers/Movie.ts
@@ -0,0 +1,12 @@
+import {BasicHandler} from "./Basic";
+
+export class MovieHandler extends BasicHandler {
+
+    async getUpcomingMovies () {
+        
+    }
+
+    async getMovieDetails() {
+
+    }
+}

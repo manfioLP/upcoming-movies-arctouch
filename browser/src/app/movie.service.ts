558d580f257654b301c5543d68c6f252d29e86f4
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2019-11-06 21:21:17 -0200

    fix service calls

--- browser/src/app/movie.service.ts
+++ browser/src/app/movie.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Movie } from '../model/Movie';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  private api_url = 'localhost:1968/api';  // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
+  };
+
+  constructor(
+    private http: HttpClient) { 
+    }
+  
+  
+
+  getUpcomingMovies(page: number): Observable<any> {
+    return this.http.get<Movie[]>(`${this.api_url}/upcoming?page=${page}`)
+      .pipe(
+        tap(res => {}))
+  }
+
+  getMovie(id: number): Observable<any> {    
+    return this.http.get<Movie[]>(`${this.api_url}/details/${id}`)
+      .pipe(
+        tap(res => {})
+      )
+  }
+
 }

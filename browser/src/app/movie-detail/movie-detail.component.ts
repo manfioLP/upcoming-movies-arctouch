96cb80e607c850b35409309848c81bad6e812f06
Leopoldo Manfio <manfio.leopoldo@gmail.com>
2019-11-29 04:14:09 -0200

    add binding prop to component

--- browser/src/app/movie-detail/movie-detail.component.ts
+++ browser/src/app/movie-detail/movie-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MovieList } from 'src/model/Movie';
 
 @Component({
   selector: 'app-movie-detail',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailComponent implements OnInit {
 
+  @Input() public movie: MovieList;
   constructor() { }
 
   ngOnInit() {
